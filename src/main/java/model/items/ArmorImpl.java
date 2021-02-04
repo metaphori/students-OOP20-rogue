2795da86ea0f3d04c1a191b8b2719fe15c9dcbe0
Luca Tassinari <luca.tassinari.2000@gmail.com>
2021-02-04 10:41:33 +0100

    Completed ArmorImpl

--- src/main/java/model/items/ArmorImpl.java
+++ src/main/java/model/items/ArmorImpl.java
@@ -1,11 +1,25 @@
 package model.items;
 
+/** 
+ * Represents an implementation for a game {@link Armor}.
+ *
+ */
 public class ArmorImpl implements Armor {
+
+    private final ArmorType armor;
+
+    /**
+     * Builds a new {@link ArmorImpl}.
+     * @param armor
+     *          the armor type
+     */
+    public ArmorImpl(final ArmorType armor) {
+        this.armor = armor;
+    }
 
     @Override
     public final int getAC() {
-        // TODO Auto-generated method stub
-        return 0;
+        return armor.getAC();
     }
 
 }
