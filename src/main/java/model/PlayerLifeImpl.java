3c738b0dc2ca3cab1e4556ea35c6d7b9a6411a31
Luca Tassinari <luca.tassinari.2000@gmail.com>
2021-02-04 16:14:11 +0100

    Added PlayerLifeImpl implementation

---
+++ src/main/java/model/PlayerLifeImpl.java
@@ -0,0 +1,44 @@
+package model;
+
+public class PlayerLifeImpl extends AbstractLife implements PlayerLife {
+
+    private int strength;
+
+    public PlayerLifeImpl(final int healthPoints, final int experience, final int strength) {
+        super(healthPoints, experience);
+        this.strength = strength;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void increaseExperience(final int increment) {
+        this.setExperience(this.getExperience() + increment);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void powerUp(final int increment) {
+        this.setHealthPoints(this.getHealthPoints() + increment);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void addStrength(final int increment) {
+        this.strength = this.strength + increment;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getStrength() {
+        return this.strength;
+    }
+
+}
