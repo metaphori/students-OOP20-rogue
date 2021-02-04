b832516c24a25f1e036387f9db7b0997e6b2ad82
Luca Tassinari <luca.tassinari.2000@gmail.com>
2021-02-04 10:31:54 +0100

    Created ArmorImpl

---
+++ src/main/java/model/items/ArmorImpl.java
@@ -0,0 +1,11 @@
+package model.items;
+
+public class ArmorImpl implements Armor {
+
+    @Override
+    public final int getAC() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
