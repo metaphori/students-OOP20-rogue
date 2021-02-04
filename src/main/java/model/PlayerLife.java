195adc8efd4251d8e98c11eda9aec656657daa02
Luca Tassinari <luca.tassinari.2000@gmail.com>
2021-02-04 16:13:22 +0100

    Added PlayerLife interface

---
+++ src/main/java/model/PlayerLife.java
@@ -0,0 +1,33 @@
+package model;
+
+public interface PlayerLife extends Life {
+
+    /**
+     * Increase the player experience.
+     * @param increment
+     *          the quantity to add to the player experience
+     */
+    void increaseExperience(int increment);
+
+    /**
+     * Increase the player health points.
+     * @param increment
+     *          the quantity to add to the player health points
+     */
+    void powerUp(int increment);
+
+    /**
+     * Increase the player strength.
+     * @param increment
+     *          the quantity to add to the player strength
+     */
+    void addStrength(int increment);
+
+    /**
+     * 
+     * @return the player strength
+     */
+    int getStrength();
+
+    // TODO hunger?
+}
