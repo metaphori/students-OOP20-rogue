5626fd9f4de11f749444da0f81e7002ae27dc6c0
Luca Tassinari <luca.tassinari.2000@gmail.com>
2021-02-04 16:12:44 +0100

    Added AbstractLife impl

---
+++ src/main/java/model/AbstractLife.java
@@ -0,0 +1,63 @@
+package model;
+
+public abstract class AbstractLife implements Life {
+
+    private int healthPoints;
+    private int experience;
+
+    protected AbstractLife(final int healthPoints, final int experience) {
+        this.healthPoints = healthPoints;
+        this.experience = experience;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void hurt(final int damage) {
+        this.healthPoints = this.healthPoints - damage;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getHealthPoints() {
+        return this.healthPoints;
+    }
+
+    /**
+     * 
+     * @param healthPoints
+     *          the healthPoints
+     */
+    protected void setHealthPoints(final int healthPoints) {
+        this.healthPoints = healthPoints;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getExperience() {
+        return this.experience;
+    }
+
+    /**
+     * 
+     * @param experience
+     *          the experience
+     */
+    protected void setExperience(final int experience) {
+        this.experience = experience;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isDead() {
+        return this.healthPoints == 0;
+    }
+
+}
