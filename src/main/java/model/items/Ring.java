c4d4b9a5cbc0e179ddc0b0eee3765c935dfe9526
Luca Tassinari <luca.tassinari.2000@gmail.com>
2021-02-04 12:23:06 +0100

    Added Ring interface

---
+++ src/main/java/model/items/Ring.java
@@ -0,0 +1,11 @@
+package model.items;
+
+/** 
+ * An interface modeling a game Ring. 
+ * A Ring is an item relatively permanent magic which adds 
+ * to the player different behaviors depending on its type.
+ * 
+ */
+public interface Ring {
+
+}
