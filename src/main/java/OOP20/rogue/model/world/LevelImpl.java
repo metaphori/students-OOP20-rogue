a2912de049d4787b46388de84ce7f71d67b46f9c
Manuel Quarneti <manuelquarneti@gmail.com>
2021-02-04 12:20:00 +0100

    fix comment formatting

--- src/main/java/OOP20/rogue/model/world/LevelImpl.java
+++ src/main/java/OOP20/rogue/model/world/LevelImpl.java
@@ -50,7 +50,9 @@ public class LevelImpl implements Level {
     }
 
     /*
-     * 0 | 1 | 2 | 3 4 | 5 | 6 | 7 8 | 9 ...
+     * 0 | 1 | 2 | 3
+     * 4 | 5 | 6 | 7
+     * 8 | 9 ...
      */
     private Vector<Tile> tileMatrix = new Vector<>();
     private int height;
