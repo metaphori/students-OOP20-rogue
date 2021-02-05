1eb3d2ea07522ec6397eb48450e2022832e9829f
pietropezzi <pietro.pezzi3@studio.unibo.it>
2021-02-05 19:34:11 +0100

    Updated Iventory interface Also updated InventoryImpl with interface mothods and javadoc

--- src/main/java/model/items/Inventory.java
+++ src/main/java/model/items/Inventory.java
@@ -2,4 +2,12 @@ package model.items;
 
 public interface Inventory {
 
+    boolean useItem(int index);
+
+    boolean useItem(Item item);
+
+    boolean getItem(int index);
+
+    boolean addItem(Item item);
+
 }
--- src/main/java/model/items/InventoryImpl.java
+++ src/main/java/model/items/InventoryImpl.java
@@ -2,4 +2,42 @@ package model.items;
 
 public class InventoryImpl implements Inventory {
 
+    /**
+     * @param index of the inventory slot to select.
+     * @return true if the item was correctly used, false otherwise.
+     */
+    public boolean useItem(final int index) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    /**
+     * @param item to search in the inventory and use.
+     * @return true if the item was correctly used, false otherwise.
+     */
+    public boolean useItem(final Item item) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    /**
+     * @param index of the inventory slot to select.
+     * @return the item contained in the given inventory slot.
+     */
+    public boolean getItem(final int index) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    /**
+     * @param item to add to the inventory.
+     * @return true if the item was correctly added to the inventory, 
+     * false if the inventory is full or if the inventory contains the
+     * limit amount for the item.
+     */
+    public boolean addItem(final Item item) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
 }
