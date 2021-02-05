01a0c32c814bb7a504e4d24c77976fc51640d23e
Luca Tassinari <luca.tassinari.2000@gmail.com>
2021-02-05 11:17:47 +0100

    Added Player impl

---
+++ src/main/java/model/creature/PlayerImpl.java
@@ -0,0 +1,11 @@
+package model.creature;
+
+public class PlayerImpl extends AbstractCreature implements Player {
+
+    // TODO Weapon, Armor, Inventory.
+
+    protected PlayerImpl(Life life) {
+        super(life);
+    }
+
+}
