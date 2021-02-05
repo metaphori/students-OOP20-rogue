cba2f080f1337e84ecd2be2c46c620afe21007bf
pietropezzi <pietro.pezzi3@studio.unibo.it>
2021-02-05 20:23:28 +0100

    Updated InventoryImpl Added constructor and updated useItem wiht index

--- src/main/java/model/items/InventoryImpl.java
+++ src/main/java/model/items/InventoryImpl.java
@@ -1,13 +1,57 @@
 package model.items;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import javafx.util.Pair;
+import model.creature.Player;
+
 public class InventoryImpl implements Inventory {
+
+    private static final int INVENTORY_SIZE = 20;
+
+    private final Player player;
+    private final Map<Integer, Pair<Optional<Item>, Integer>> inventory = new HashMap<>(INVENTORY_SIZE);
+
+    public InventoryImpl(final Player player) {
+        this.player = player;
+        for (int i = 1; i <= INVENTORY_SIZE; i++) {
+            this.inventory.put(i, new Pair<>(Optional.empty(), 0));
+        }
+    }
 
     /**
      * @param index of the inventory slot to select.
      * @return true if the item was correctly used, false otherwise.
      */
     public boolean useItem(final int index) {
-        // TODO Auto-generated method stub
+        if (inventory.containsKey(index)) {
+            if (inventory.get(index).getKey().isPresent()) {
+                /*
+                 * Save to use Item.
+                 */
+                final Item toUse = inventory.get(index).getKey().get();
+                /*
+                 * Update Inventory.
+                 */
+                final int amount = inventory.get(index).getValue();
+                if (amount == 1) {
+                    //Last of that Item in inventory, make the optional empty.
+                    inventory.put(index, new Pair<>(Optional.empty(), 0));
+                } else {
+                    //Lower amount of item by one after use.
+                    inventory.put(index, new Pair<>(Optional.of(toUse), amount - 1));
+                }
+                /*
+                 * Use item
+                 */
+                return toUse.use(this.player);
+            } else {
+                return false;
+            }
+        }
+        //TODO Add OutOfInventoryException
         return false;
     }
 
