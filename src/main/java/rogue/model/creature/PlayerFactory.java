facb705f6463007c6b7cf2f5765abb4474971aca
Luca Tassinari <luca.tassinari10@studio.unibo.it>
2021-02-06 17:49:44 +0100

    Added the player factory interface

---
+++ src/main/java/rogue/model/creature/PlayerFactory.java
@@ -0,0 +1,23 @@
+package rogue.model.creature;
+
+/**
+ * An interface modeling a factory for the {@link Player}.
+ * 
+ */
+public interface PlayerFactory {
+
+    /**
+     * Creates a new {@link Player} with defaults life settings.
+     * @return a Player
+     */
+    Player create();
+
+    /**
+     * Creates a new {@link Player} with the given life.
+     * @param life
+     *          the player life
+     * @return a PLayer
+     */
+    Player createByLife(PlayerLife life);
+
+}
