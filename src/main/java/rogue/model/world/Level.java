a623959de38de5485d90f783a89b9b8f13166dec
Manuel Quarneti <manuelquarneti@gmail.com>
2021-02-06 17:40:16 +0100

    fix

--- src/main/java/rogue/model/world/Level.java
+++ src/main/java/rogue/model/world/Level.java
@@ -8,6 +8,6 @@ public interface Level {
     Stream<Tile> getTileStream();
     int distance(Tile t1, Tile t2);
     void moveEntity(Entity e, Tile t) throws CannotMoveException;
-    void shiftEntity(Entity e, Direction d, int i);
+    void shiftEntity(Entity e, Direction d, int i) throws CannotMoveException;
     void removeEntity(Entity e) throws CannotRemoveException;
 }
