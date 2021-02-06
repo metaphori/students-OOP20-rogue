b4317f2553b2135f2fadb613d566faba5396b930
FabioDrago13 <55588165+FabioDrago13@users.noreply.github.com>
2021-02-06 18:20:23 +0100

    Create MonsterLife.java

---
+++ src/main/java/rogue/model/creature/MonsterLife.java
@@ -0,0 +1,10 @@
+package rogue.model.creature;
+
+public class MonsterLife extends AbstractLife {
+
+	protected MonsterLife(int healthPoints, int experience) {
+		super(healthPoints, experience);
+		// TODO Auto-generated constructor stub
+	}
+
+}
