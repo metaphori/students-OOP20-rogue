4ca710b2ac4fd19abfd6ec9712662355fdc3e4c3
Luca Tassinari <luca.tassinari10@studio.unibo.it>
2021-02-07 16:29:43 +0100

    Fixed a type error in the constructor parameter

--- src/main/java/rogue/model/items/weapons/IncreaseAccuracy.java
+++ src/main/java/rogue/model/items/weapons/IncreaseAccuracy.java
@@ -7,7 +7,7 @@ public class IncreaseAccuracy extends WeaponDecorator {
 
     private static final int ADDITIONAL_ACCURACY = 2;
 
-    public IncreaseAccuracy(final BaseWeapon weapon) {
+    public IncreaseAccuracy(final Weapon weapon) {
         super(weapon);
     }
 
