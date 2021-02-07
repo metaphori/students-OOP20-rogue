e166a738db74b67ff4d2635a5917609172aaabae
Fabio Chiarini <fabio.chiarini@studio.unibo.it>
2021-02-07 16:48:45 +0100

    Approved Checkstyle

--- src/main/java/rogue/model/creature/MonsterLife.java
+++ src/main/java/rogue/model/creature/MonsterLife.java
@@ -1,10 +1,12 @@
 package rogue.model.creature;
 
+/**
+ * An interface modeling a specific {@link Life} for the {@link Monster}.
+ *
+ */
 public class MonsterLife extends AbstractLife {
 
-	protected MonsterLife(int healthPoints, int experience) {
-		super(healthPoints, experience);
-		// TODO Auto-generated constructor stub
-	}
-
+    protected MonsterLife(final int healthPoints, final int experience) {
+        super(healthPoints, experience);
+    }
 }
