d9047ede8c6d473d413ae0a02dc294ad72195544
pietropezzi <pietro.pezzi3@studio.unibo.it>
2021-02-07 20:21:36 +0100

    Updated Inventory interface Javadoc

--- src/main/java/rogue/model/items/inventory/Inventory.java
+++ src/main/java/rogue/model/items/inventory/Inventory.java
@@ -4,12 +4,35 @@ import java.util.Optional;
 
 import rogue.model.items.Item;
 
+/**
+ * An interface for modeling a game Inventory.
+ *
+ */
 public interface Inventory {
 
+    /**
+     * Use the item contained in the index slot.
+     * @param index of the slot to select.
+     * @return true if the item was correctly used, false if 
+     * item cannot be consumed.
+     * @throws OutOfInventoryException if given an invalid index.
+     */
     boolean useItem(int index) throws OutOfInventoryException;
 
+    /**
+     * Get the item contained in the index slot of the inventory.
+     * @param index of the wanted slot.
+     * @return Optional.empty if given slot is empty, Optional.of(Item)
+     * otherwise.
+     */
     Optional<Item> getItem(int index);
 
+    /**
+     * Add an item to the inventory.
+     * @param item to add to the inventory.
+     * @return true if item was correctly added, false if
+     * the inventory is full.
+     */
     boolean addItem(Item item);
 
 }
