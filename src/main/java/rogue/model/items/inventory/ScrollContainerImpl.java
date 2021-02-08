b8a525ef970e7a76a05d4ef4b70a95a4e80d5b25
pietropezzi <pietro.pezzi3@studio.unibo.it>
2021-02-08 19:50:32 +0100

    Created ScrollContainerImpl

---
+++ src/main/java/rogue/model/items/inventory/ScrollContainerImpl.java
@@ -0,0 +1,94 @@
+package rogue.model.items.inventory;
+
+import java.util.Optional;
+
+import javafx.util.Pair;
+import rogue.model.creature.Player;
+import rogue.model.items.scroll.Scroll;
+
+public class ScrollContainerImpl implements ScrollContainer {
+
+    private Pair<Optional<Scroll>, Integer> scroll;
+    private final Player player;
+
+    public ScrollContainerImpl(final Player player) {
+        this.player = player;
+    }
+
+    /**
+     * @param scroll to activate 
+     */
+    public void activateScroll(final Scroll scroll) {
+        /*
+         * Use the removeActiveScroll method to remove eventual active scroll.
+         * Then activate the given scroll.
+         */
+        removeActiveScroll();
+        this.scroll = new Pair<>(Optional.of(scroll), scroll.getEffectDuration());
+    }
+
+    /**
+     * @return true if correctly removed active scroll, false if there's
+     * no scroll to remove.
+     */
+    public boolean removeActiveScroll() {
+        if (this.scroll.getKey().isEmpty()) {
+            /*
+             * No scroll to remove.
+             */
+            return false;
+        } else {
+            /*
+             * Before removing the scroll, remove the scroll's effect.
+             */
+            this.scroll.getKey().get().remove(this.player);
+            /*
+             * Remove the scroll.
+             */
+            this.scroll = new Pair<>(Optional.empty(), 0);
+            return true;
+        }
+    }
+
+    /**
+     * @return Currently active scroll.
+     */
+    public Optional<Scroll> getActiveScroll() {
+        return this.scroll.getKey();
+    }
+
+    /**
+     * @param amount to subtract to scroll's duration
+     * @return true if correctly updated the duration, false if
+     * there's no active scroll.
+     */
+    public boolean updateEffectDuration(final int amount) {
+        /*
+         * Check if active Scroll
+         */
+        if (!this.scroll.getKey().isEmpty()) {
+            /*
+             * Check if amount will remove the scroll.
+             */
+            if (this.scroll.getValue() - amount <= 0) {
+                /*
+                 * Scroll duration is over, remove scroll.
+                 */
+                this.scroll = new Pair<>(Optional.empty(), 0);
+                return true;
+            } else {
+                /*
+                 * Update scroll's duration
+                 */
+                this.scroll = new Pair<>(this.scroll.getKey(), this.scroll.getValue() - amount);
+                return true;
+            }
+        } else {
+            /*
+             * No active scroll, nothing to update
+             */
+            return false;
+        }
+    }
+
+}
