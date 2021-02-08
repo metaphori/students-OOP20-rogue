367510c4c56eb6058134de720f08a801313f6b63
Luca Tassinari <luca.tassinari10@studio.unibo.it>
2021-02-08 13:05:37 +0100

    Updated entirely the new test for rings behaviours

--- src/test/java/rogue/model/items/rings/RingsTest.java
+++ src/test/java/rogue/model/items/rings/RingsTest.java
@@ -1,75 +1,120 @@
 package rogue.model.items.rings;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
 import org.junit.Test;
 
-import rogue.model.items.potion.PotionType;
-
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertEquals;
+import rogue.model.creature.Player;
+import rogue.model.creature.PlayerFactoryImpl;
+import rogue.model.items.armor.ArmorImpl;
+import rogue.model.items.armor.ArmorType;
+import rogue.model.items.weapons.BaseWeapon;
+import rogue.model.items.weapons.IncreaseAccuracy;
+import rogue.model.items.weapons.IncreaseDamage;
+import rogue.model.items.weapons.WeaponType;
 
 public class RingsTest {
-    private static final int POTION_I_MAX = 10;
-    private static final int POTION_I_MIN = 5;
-    private static final int POTION_II_MAX = 20;
-    private static final int POTION_II_MIN = 15;
-    private static final int POTION_III_MAX = 33;
-    private static final int POTION_III_MIN = 25;
-    private static final int POTION_IV_MAX = 50;
-    private static final int POTION_IV_MIN = 40;
-    private static final int POTION_V_VALUE = 100;
-    private static final int CORRUPT_POTION_I_MAX = -10;
-    private static final int CORRUPT_POTION_I_MIN = -5;
-    private static final int CORRUPT_POTION_II_MAX = -20;
-    private static final int CORRUPT_POTION_II_MIN = -15;
 
-    private static final String TOO_HIGH = "too high";
-    private static final String TOO_LOW = "too low";
+    private final Player player = new PlayerFactoryImpl().create();
 
-    @Test
-    public void testPotionI() {
-        final int test = PotionType.POTION_I.getHpValue();
-        assertTrue(TOO_HIGH, test <= POTION_I_MAX);
-        assertTrue(TOO_LOW, test >= POTION_I_MIN);
+    @Before
+    public void initTest() {
+        assertEquals(new ArmorImpl(ArmorType.LEATHER), player.getEquipment().getArmor());
+        assertEquals(new BaseWeapon(WeaponType.MACE), player.getEquipment().getWeapon());
     }
 
     @Test
-    public void testPotionII() {
-        final int test = PotionType.POTION_II.getHpValue();
-        assertTrue(TOO_HIGH, test <= POTION_II_MAX);
-        assertTrue(TOO_LOW, test >= POTION_II_MIN);
+    public void testBasic1() {
+        // Improves weapon accuracy
+        final Ring ring = new RingImpl(RingType.DEXTERITY);
+        assertTrue(ring.use(player));
+        assertEquals(new IncreaseAccuracy(new BaseWeapon(WeaponType.MACE)), player.getEquipment().getWeapon());
+        this.player.getEquipment().detachRing();
+        assertEquals(new BaseWeapon(WeaponType.MACE), player.getEquipment().getWeapon());
     }
 
     @Test
-    public void testPotionIII() {
-        final int test = PotionType.POTION_III.getHpValue();
-        assertTrue(TOO_HIGH, test <= POTION_III_MAX);
-        assertTrue(TOO_LOW, test >= POTION_III_MIN);
+    public void testBasic2() {
+        // Improves armor AC
+        final Ring ring = new RingImpl(RingType.PROTECTION);
+        assertTrue(ring.use(player));
+        assertEquals(10, player.getEquipment().getArmor().getAC());
+        this.player.getEquipment().detachRing();
+        assertEquals(8, player.getEquipment().getArmor().getAC());
     }
 
     @Test
-    public void testPotionIV() {
-        final int test = PotionType.POTION_IV.getHpValue();
-        assertTrue(TOO_HIGH, test <= POTION_IV_MAX);
-        assertTrue(TOO_LOW, test >= POTION_IV_MIN);
+    public void testChangeWeapon() {
+        // Improves weapon accuracy
+        final Ring ring = new RingImpl(RingType.DEXTERITY);
+        assertTrue(ring.use(player));
+        // change the weapon currently in use, so i expect the dexterity ring is now applied to arrow 
+        player.getEquipment().setWeapon(new BaseWeapon(WeaponType.ARROW));
+        assertEquals(new IncreaseAccuracy(new BaseWeapon(WeaponType.ARROW)), player.getEquipment().getWeapon());
+        assertEquals(new ArmorImpl(ArmorType.LEATHER), player.getEquipment().getArmor());
+        this.player.getEquipment().detachRing();
+        assertEquals(new BaseWeapon(WeaponType.ARROW), player.getEquipment().getWeapon());
+        assertEquals(new ArmorImpl(ArmorType.LEATHER), player.getEquipment().getArmor());
     }
 
     @Test
-    public void testPotionV() {
-        final int test = PotionType.POTION_V.getHpValue();
-        assertEquals(test, POTION_V_VALUE);
+    public void testChangeWeapon1() {
+        // Improves armor AC
+        final Ring ring = new RingImpl(RingType.PROTECTION);
+        assertTrue(ring.use(player));
+        assertEquals(new BaseWeapon(WeaponType.MACE), player.getEquipment().getWeapon());
+        assertEquals(10, player.getEquipment().getArmor().getAC());
+        player.getEquipment().setWeapon(new BaseWeapon(WeaponType.ARROW));
+        assertEquals(new BaseWeapon(WeaponType.ARROW), player.getEquipment().getWeapon());
+        assertEquals(10, player.getEquipment().getArmor().getAC());
+        this.player.getEquipment().detachRing();
+        assertEquals(8, player.getEquipment().getArmor().getAC());
     }
 
     @Test
-    public void testCorruptPotionI() {
-        final int test = PotionType.CORRUPT_POTION_I.getHpValue();
-        assertTrue(TOO_HIGH, test >= CORRUPT_POTION_I_MAX);
-        assertTrue(TOO_LOW, test <= CORRUPT_POTION_I_MIN);
+    public void testChangeArmor() {
+        // Improves weapon accuracy
+        final Ring ring = new RingImpl(RingType.DEXTERITY);
+        assertTrue(ring.use(player));
+        // change the armor -> does not produce any effects!
+        player.getEquipment().setArmor(new ArmorImpl(ArmorType.CHAIN_MAIL));
+        assertEquals(new IncreaseAccuracy(new BaseWeapon(WeaponType.MACE)), player.getEquipment().getWeapon());
+        assertEquals(new ArmorImpl(ArmorType.CHAIN_MAIL), player.getEquipment().getArmor());
+        this.player.getEquipment().detachRing();
+        assertEquals(new BaseWeapon(WeaponType.MACE), player.getEquipment().getWeapon());
+        assertEquals(new ArmorImpl(ArmorType.CHAIN_MAIL), player.getEquipment().getArmor());
     }
 
     @Test
-    public void testCorruptPotionII() {
-        final int test = PotionType.CORRUPT_POTION_II.getHpValue();
-        assertTrue(TOO_HIGH, test >= CORRUPT_POTION_II_MAX);
-        assertTrue(TOO_LOW, test <= CORRUPT_POTION_II_MIN);
+    public void testChangeArmor1() {
+        // Improves armor AC
+        final Ring ring = new RingImpl(RingType.PROTECTION);
+        assertTrue(ring.use(player));
+        assertEquals(10, player.getEquipment().getArmor().getAC());
+        // change the armor currently in use, so i expect the protection ring is now applied to the new armor 
+        player.getEquipment().setArmor(new ArmorImpl(ArmorType.CHAIN_MAIL));
+        assertEquals(7, player.getEquipment().getArmor().getAC());
     }
+
+    @Test
+    public void testReplace() {
+        final Ring ring = new RingImpl(RingType.DEXTERITY);
+        assertTrue(ring.use(player));
+        assertEquals(new IncreaseAccuracy(new BaseWeapon(WeaponType.MACE)), player.getEquipment().getWeapon());
+        assertEquals(new ArmorImpl(ArmorType.LEATHER), player.getEquipment().getArmor());
+
+        final Ring ring2 = new RingImpl(RingType.INCREASE_DAMAGE);
+        assertFalse(ring2.use(player));
+        assertEquals(new IncreaseAccuracy(new BaseWeapon(WeaponType.MACE)), player.getEquipment().getWeapon());
+        assertEquals(new ArmorImpl(ArmorType.LEATHER), player.getEquipment().getArmor());
+
+        assertTrue(this.player.getEquipment().detachRing());
+        assertTrue(ring2.use(player));
+        assertEquals(new IncreaseDamage(new BaseWeapon(WeaponType.MACE)), player.getEquipment().getWeapon());
+        assertEquals(new ArmorImpl(ArmorType.LEATHER), player.getEquipment().getArmor());
+    }
+
 }
