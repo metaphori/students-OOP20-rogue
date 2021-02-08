43f29cf4dc8306766adb9b0b666817fb3cf7dbdd
Luca Tassinari <luca.tassinari10@studio.unibo.it>
2021-02-08 09:48:10 +0100

    Completed with consumers and utility methods

--- src/main/java/rogue/model/items/rings/RingType.java
+++ src/main/java/rogue/model/items/rings/RingType.java
@@ -1,50 +1,48 @@
 package rogue.model.items.rings;
 
-// import java.util.function.Consumer;
+import java.util.function.Consumer;
+
+import rogue.model.items.weapons.IncreaseAccuracy;
+import rogue.model.items.weapons.IncreaseDamage;
+
+import rogue.model.creature.Player;
 
 /**
  * Represents an enumeration for declaring ring types.
  * 
- * The first field keeps track the percentage of chance that
- * the corresponding ring has of being found.
- * The second field keeps track 
- *
+ * The first field keeps track the percentage of chance that the corresponding ring has of being found.
+ * The second field keeps track a consumer describing its effect on the player.
  * TODO to finish
  */
 public enum RingType {
 
     /**
-     * Adds to strength.
-     */
-    ADD_STRENGTH(9),
-    /**
      * Increase armor AC.
      */
-    PROTECTION(9),
-    /**
-     * Worth 10 gold.
-     */
-    ADORNMENT(1),
+    PROTECTION(9, p -> p.getEquipment().getArmor().increaseAC(2)),
     /**
      * Improves weapon accuracy.
      */
-    DEXTERITY(8),
+    DEXTERITY(8, p -> p.getEquipment().setWeapon(new IncreaseAccuracy(p.getEquipment().getWeapon()))),
     /**
      * Increases weapon damage.
      */
-    INCREASE_DAMAGE(8),
-    /** 
-     * Heals 1 health points per turn.
-     */
-    REGENERATION(4);
+    INCREASE_DAMAGE(8, p -> p.getEquipment().setWeapon(new IncreaseDamage(p.getEquipment().getWeapon())));
 
     private final int chance;
-    // TODO Change Creature with Player
-    // private final Consumer<Creature> consumer;
+    private final Consumer<Player> consumer;
 
-    RingType(final int chance) {
+    RingType(final int chance, final Consumer<Player> consumer) {
         this.chance = chance;
-        // this.consumer = consumer;
+        this.consumer = consumer;
+    }
+
+    protected Consumer<Player> getConsumer() {
+        return this.consumer;
+    }
+
+    protected int getChance() {
+        return this.chance;
     }
 
 }
