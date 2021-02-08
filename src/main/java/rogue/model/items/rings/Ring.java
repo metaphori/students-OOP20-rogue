6116ee68b784b00e2f122c71ad3e7e33849eb0d1
Luca Tassinari <luca.tassinari10@studio.unibo.it>
2021-02-08 09:45:27 +0100

    Added getChance method

--- src/main/java/rogue/model/items/rings/Ring.java
+++ src/main/java/rogue/model/items/rings/Ring.java
@@ -1,11 +1,18 @@
 package rogue.model.items.rings;
 
+import rogue.model.items.Item;
+
 /** 
  * An interface modeling a game Ring. 
  * A Ring is an item relatively permanent magic which adds 
  * to the player different behaviors depending on its type.
  * 
  */
-public interface Ring {
+public interface Ring extends Item {
+
+    /**
+     * @return the ring chance
+     */
+    int getChance();
 
 }
