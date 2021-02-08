c346e45b16d92577c94e84c7d3e105304c9c83fc
pietropezzi <pietro.pezzi3@studio.unibo.it>
2021-02-08 22:42:30 +0100

    Completed ScrollImplTest

--- src/test/java/rogue/model/items/scroll/ScrollImplTest.java
+++ src/test/java/rogue/model/items/scroll/ScrollImplTest.java
@@ -6,7 +6,6 @@ import rogue.model.creature.PlayerImpl;
 import rogue.model.creature.PlayerLifeImpl;
 
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertEquals;
 
 public class ScrollImplTest {
