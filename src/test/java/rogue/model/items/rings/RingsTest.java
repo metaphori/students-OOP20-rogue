53f30497412277309a866e07fbce128eb15a4865
Luca Tassinari <luca.tassinari10@studio.unibo.it>
2021-02-09 22:26:20 +0100

    Adjusted and added a new test

--- src/test/java/rogue/model/items/rings/RingsTest.java
+++ src/test/java/rogue/model/items/rings/RingsTest.java
@@ -32,7 +32,9 @@ public class RingsTest {
         final Ring ring = new RingImpl(RingType.DEXTERITY);
         assertTrue(ring.use(player));
         assertEquals(new IncreaseAccuracy(new BaseWeapon(WeaponType.MACE)), player.getEquipment().getWeapon());
+        // detaches the ring
         ring.stopUsing(player);
+        // the weapon is back to normal state
         assertEquals(new BaseWeapon(WeaponType.MACE), player.getEquipment().getWeapon());
     }
 
@@ -42,7 +44,9 @@ public class RingsTest {
         final Ring ring = new RingImpl(RingType.PROTECTION);
         assertTrue(ring.use(player));
         assertEquals(10, player.getEquipment().getArmor().getAC());
+        // detaches the ring
         ring.stopUsing(player);
+        // the armor is back to normal state
         assertEquals(8, player.getEquipment().getArmor().getAC());
     }
 
@@ -67,6 +71,7 @@ public class RingsTest {
         assertTrue(ring.use(player));
         assertEquals(new BaseWeapon(WeaponType.MACE), player.getEquipment().getWeapon());
         assertEquals(10, player.getEquipment().getArmor().getAC());
+        // change the weapon does't affect the ring behavior
         player.getEquipment().setWeapon(new BaseWeapon(WeaponType.ARROW));
         assertEquals(new BaseWeapon(WeaponType.ARROW), player.getEquipment().getWeapon());
         assertEquals(10, player.getEquipment().getArmor().getAC());
@@ -105,16 +110,37 @@ public class RingsTest {
         assertTrue(ring.use(player));
         assertEquals(new IncreaseAccuracy(new BaseWeapon(WeaponType.MACE)), player.getEquipment().getWeapon());
         assertEquals(new ArmorImpl(ArmorType.LEATHER), player.getEquipment().getArmor());
-
+        // create a new increase damage ring
         final Ring ring2 = new RingImpl(RingType.INCREASE_DAMAGE);
+        // the dexterity ring is already applied. Is impossible to wear a new ring until first detaching the dexterity one.
         assertFalse(ring2.use(player));
+        // Indeed all remained the same
         assertEquals(new IncreaseAccuracy(new BaseWeapon(WeaponType.MACE)), player.getEquipment().getWeapon());
         assertEquals(new ArmorImpl(ArmorType.LEATHER), player.getEquipment().getArmor());
-
+        // detaching the dexterity ring
         ring.stopUsing(player);
+        // now the increase damage ring can be worn
         assertTrue(ring2.use(player));
         assertEquals(new IncreaseDamage(new BaseWeapon(WeaponType.MACE)), player.getEquipment().getWeapon());
         assertEquals(new ArmorImpl(ArmorType.LEATHER), player.getEquipment().getArmor());
+    }
+
+    @Test
+    public void anotherTest() {
+        final Ring ring = new RingImpl(RingType.DEXTERITY);
+        player.getEquipment().setWeapon(new BaseWeapon(WeaponType.SHURIKEN));
+        player.getEquipment().setArmor(new ArmorImpl(ArmorType.SPLINT_MAIL));
+        assertTrue(ring.use(player));
+        assertEquals(new IncreaseAccuracy(new BaseWeapon(WeaponType.SHURIKEN)), player.getEquipment().getWeapon());
+        assertEquals(new ArmorImpl(ArmorType.SPLINT_MAIL), player.getEquipment().getArmor());
+        player.getEquipment().setWeapon(new BaseWeapon(WeaponType.SHORT_BOW));
+        player.getEquipment().setWeapon(new BaseWeapon(WeaponType.DAGGER));
+        assertEquals(new IncreaseAccuracy(new BaseWeapon(WeaponType.DAGGER)), player.getEquipment().getWeapon());
+        player.getEquipment().setArmor(new ArmorImpl(ArmorType.PLATE_MAIL));
+        assertEquals(new ArmorImpl(ArmorType.PLATE_MAIL), player.getEquipment().getArmor());
+        ring.stopUsing(player);
+        assertEquals(new ArmorImpl(ArmorType.PLATE_MAIL), player.getEquipment().getArmor());
+        assertEquals(new BaseWeapon(WeaponType.DAGGER), player.getEquipment().getWeapon());
     }
 
 }
