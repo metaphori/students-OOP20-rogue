628e73dc97ae46ce9702b0c6cf101050045373da
Luca Tassinari <luca.tassinari10@studio.unibo.it>
2021-02-09 16:55:58 +0100

    Updated use and stopUsing implementation with memento

--- src/main/java/rogue/model/items/rings/RingImpl.java
+++ src/main/java/rogue/model/items/rings/RingImpl.java
@@ -2,33 +2,36 @@ package rogue.model.items.rings;
 
 import rogue.model.creature.Player;
 import rogue.model.items.Equipment;
+import rogue.model.items.Memento;
 
-public class RingImpl implements Ring {
+public final class RingImpl implements Ring {
 
+    private Memento memento;
     private final RingType ring;
 
     public RingImpl(final RingType ring) {
         this.ring = ring;
     }
 
-    public void consume(final Equipment equipment) {
-        this.ring.getConsumer().accept(equipment);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public boolean use(final Player player) {
-        return player.getEquipment().attachRing(this);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public int getChance() {
         return this.ring.getChance();
+    }
+
+    @Override
+    public boolean use(final Player player) {
+        if (player.getEquipment().getRing().isEmpty()) {
+            this.memento = player.getEquipment().save();
+            this.ring.getConsumer().accept(player.getEquipment());
+            player.getEquipment().attachRing(this);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void stopUsing(final Player player) {
+        player.getEquipment().detachRing(this.memento);
     }
 
 }
