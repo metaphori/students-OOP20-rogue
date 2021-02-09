9f2782ceaa8e38fcad899a3b1d8da04bc83bb478
pietropezzi <pietro.pezzi3@studio.unibo.it>
2021-02-09 18:07:51 +0100

    Added testAddTwoItemScroll test

--- src/test/java/rogue/model/items/inventory/InventoryImplTest.java
+++ src/test/java/rogue/model/items/inventory/InventoryImplTest.java
@@ -7,6 +7,9 @@ import rogue.model.creature.PlayerLifeImpl;
 import rogue.model.items.potion.Potion;
 import rogue.model.items.potion.PotionImpl;
 import rogue.model.items.potion.PotionType;
+import rogue.model.items.scroll.Scroll;
+import rogue.model.items.scroll.ScrollImpl;
+import rogue.model.items.scroll.ScrollType;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
@@ -20,7 +23,7 @@ public class InventoryImplTest {
     private PlayerImpl pl;
 
     @Test
-    public void testAddItemAndGetItem() {
+    public void testAddItemAndGetItemPotion() {
         pl = new PlayerImpl(new PlayerLifeImpl.Builder().build());
         final Inventory inv = new InventoryImpl(pl);
         final Potion potion = new PotionImpl(PotionType.POTION_I);
@@ -36,5 +39,38 @@ public class InventoryImplTest {
         assertTrue(inv.getItem(1).isPresent());
         assertFalse(inv.getItem(2).isPresent());
         assertEquals(potion, inv.getItem(1).get());
+    }
+
+    @Test
+    public void testAddTwoItemScroll() {
+        pl = new PlayerImpl(new PlayerLifeImpl.Builder().build());
+        final Inventory inv = new InventoryImpl(pl);
+        final Scroll scroll = new ScrollImpl(ScrollType.SCROLL_II);
+        final Scroll scroll2 = new ScrollImpl(ScrollType.SCROLL_V);
+        /*
+         * Add item to inventory, expect true and correct
+         * inventory update.
+         */
+        try {
+            assertTrue(inv.addItem(scroll));
+        } catch (InventoryIsFullException e) {
+            e.printStackTrace();
+        }
+        assertTrue(inv.getItem(1).isPresent());
+        assertFalse(inv.getItem(2).isPresent());
+        assertEquals(scroll, inv.getItem(1).get());
+        /*
+         * Add a different scroll, expect different item
+         * to be stored in 2 slot.
+         */
+        try {
+            assertTrue(inv.addItem(scroll2));
+        } catch (InventoryIsFullException e) {
+            e.printStackTrace();
+        }
+        assertTrue(inv.getItem(1).isPresent());
+        assertEquals(scroll, inv.getItem(1).get());
+        assertTrue(inv.getItem(2).isPresent());
+        assertEquals(scroll2, inv.getItem(2).get());
     }
 }
