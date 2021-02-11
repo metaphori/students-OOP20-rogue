5222554df9c7eac29785bc741f55e9e3f7e80943
pietropezzi <pietro.pezzi3@studio.unibo.it>
2021-02-11 09:42:39 +0100

    Updated Launcher and GameController Images now work!

--- src/main/java/rogue/Launcher.java
+++ src/main/java/rogue/Launcher.java
@@ -4,21 +4,23 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Launcher extends Application {
 
     private static final int SCENE_WIDTH = 800;
     private static final int SCENE_HEIGHT = 600;
 
     /**
-     * 
+     * Create Menu.
      */
     @Override
     public void start(final Stage primaryStage) throws Exception {
         final Parent root = FXMLLoader.load(ClassLoader.getSystemResource("layout/MainMenu.fxml"));
         final Scene scene = new Scene(root, SCENE_WIDTH, SCENE_HEIGHT);
         primaryStage.setScene(scene);
+        primaryStage.getIcons().add(new Image(ClassLoader.getSystemResource("images/rogueIcon.png").toExternalForm()));
         primaryStage.setTitle("Rogue");
         primaryStage.setResizable(false);
         primaryStage.show();
--- src/main/java/rogue/controller/GameControllerImpl.java
+++ src/main/java/rogue/controller/GameControllerImpl.java
@@ -2,31 +2,47 @@ package rogue.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.text.Text;
 
-public class GameControllerImpl {
+public class GameControllerImpl implements Initializable {
 
+    @FXML private Text insertNameText;
+    @FXML private TextField nameTextField;
+    private String playerName;
+
+    /**
+     * 
+     * @param event
+     */
     @FXML
-    private ResourceBundle resources;
+    public void onNameEnter(final KeyEvent event) {
+        if (event.getCode().equals(KeyCode.ENTER)) {
+            if (nameTextField.getText() != null && !(nameTextField.getText().isEmpty())) {
+                this.playerName = nameTextField.getText();
+                System.out.println("Player name is: " + this.playerName);
+            } else {
+                insertNameText.setText("PLEASE ENTER A VALID NAME");
+            }
+        }
+    }
 
-    @FXML
-    private URL location;
-
-    @FXML
-    private AnchorPane rogueMenuImage;
-
-    @FXML
-    private TextField nameTextField;
+    /**
+     * @return player's name
+     */
+    public String getPlayerName() {
+        return this.playerName;
+    }
 
     /**
      * 
      */
-    @FXML
-    void initialize() {
-        assert rogueMenuImage != null : "fx:id=\"rogueMenuImage\" was not injected: check your FXML file 'sample.fxml'.";
-        assert nameTextField != null : "fx:id=\"nameTextField\" was not injected: check your FXML file 'sample.fxml'.";
-
+    @Override
+    public void initialize(final URL location, final ResourceBundle resources) {
     }
 }
