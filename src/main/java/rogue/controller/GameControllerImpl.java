2f1cbe980384767a590c83c8678b38d1b7292646
pietropezzi <pietro.pezzi3@studio.unibo.it>
2021-02-11 10:11:06 +0100

    Updated GameControllerImpl Added validName method

--- src/main/java/rogue/controller/GameControllerImpl.java
+++ src/main/java/rogue/controller/GameControllerImpl.java
@@ -8,11 +8,25 @@ import javafx.scene.text.Text;
 
 public class GameControllerImpl implements GameController {
 
+    private static final String NO_NAME_MESSAGE = "PLEASE ENTER A NAME";
     private static final String INVALID_NAME_MESSAGE = "PLEASE ENTER A VALID NAME";
+    private static final int NAME_MAX_LENGTH = 15;
 
     @FXML private Text insertNameText;
     @FXML private TextField nameTextField;
     private String playerName;
+
+    private boolean validName(final String name) {
+        if (name.length() <= NAME_MAX_LENGTH) {
+            for (int i = 0; i < name.length(); i++) {
+                if (name.charAt(i) == ' ') {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
 
     /**
      * Text area event to get player's name.
@@ -22,13 +36,16 @@ public class GameControllerImpl implements GameController {
     public void onNameEnter(final KeyEvent event) {
         if (event.getCode().equals(KeyCode.ENTER)) {
             if (nameTextField.getText() != null && !(nameTextField.getText().isEmpty())) {
-                this.playerName = nameTextField.getText();
-                /*
-                 * Valid name entered, start game.
-                 */
-                start();
-            } else {
+                if (validName(nameTextField.getText())) {
+                    this.playerName = nameTextField.getText();
+                    /*
+                     * Valid name entered, start game.
+                     */
+                    start();
+                }
                 insertNameText.setText(INVALID_NAME_MESSAGE);
+            } else {
+                insertNameText.setText(NO_NAME_MESSAGE);
             }
         }
     }
