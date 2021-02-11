49a8e00e11bbc1e07265688dc72813088e1e2390
Luca Tassinari <luca.tassinari10@studio.unibo.it>
2021-02-11 14:04:08 +0100

    Added StatusBar interface and implementation skeleton

---
+++ src/main/java/rogue/view/StatusBarViewImpl.java
@@ -0,0 +1,41 @@
+package rogue.view;
+
+public final class StatusBarViewImpl implements StatusBarView {
+
+    @Override
+    public void setMaxHealthPoints(int max) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void setCurrentHealthPoints(int healthPoints) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void setCoins(int coins) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void setLevel(int level) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void setStrength(int strength) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void setExperience(int experience) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
