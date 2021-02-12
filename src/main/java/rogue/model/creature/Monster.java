a7884cb29ec18f71b96963e2e977bb7e0c0b5da0
Fabio Chiarini <fabio.chiarini@studio.unibo.it>
2021-02-12 10:59:49 +0100

    Update Monster.java

--- src/main/java/rogue/model/creature/Monster.java
+++ src/main/java/rogue/model/creature/Monster.java
@@ -1,6 +1,7 @@
 package rogue.model.creature;
 
 import javafx.util.Pair;
+import rogue.model.items.potion.PotionImpl;
 
 /**
  * An interface modeling the enemy/monster.
@@ -8,40 +9,58 @@ import javafx.util.Pair;
  */
 public interface Monster extends Creature<MonsterLife> {
 
-   /**
-    *
-    *
-    * @return the type of the monster
-    */
+    /**
+     *
+     *
+     * @return the type of the monster
+     */
     MonsterType getMonsterType();
 
-   /**
-    * 
-    * @return the Armor Class of the monster
-    */
-     int getAC();
+    /**
+     * 
+     * @return the Armor Class of the monster
+     */
+    int getAC();
 
-   /**
-    * 
-    * @return the maximum damage the monster can do the minimum is 1 
-    */
+    /**
+     * 
+     * @return the maximum damage the monster can do the minimum is 1 
+     */
     Pair<Integer, Integer> getDamage();
 
-   /**
-    * 
-    * @return the monster's special skill
-    */
+    /**
+     * 
+     * @return the monster's special skill
+     */
     Special getSpecial();
 
-   /**
-    * 
-    * @return the amount of money of the monster 
-    */
+    /**
+     * 
+     * @return the amount of money of the monster 
+     */
     int getMoney();
 
+    /**
+     * 
+     * @return the monster's item
+     */
+    PotionImpl getItem();
+
+    /**
+     * 
+     * @return the item's possibility to drop
+     */
+    int getItemChange();
+
    /**
-    * 
-    * @return the monster's item
+    *
+    * @return the damage that the monster does for that single attack 
     */
-    /*Item getItem();*/
+    int attackDamage();
+
+    /**
+     * 
+     * @return or not the monster's item
+     */
+    PotionImpl dropItem();
 }
