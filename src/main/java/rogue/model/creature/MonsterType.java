9b5fb58fbf3a6aeaff79164141e0bd7e8febc6c6
Fabio Chiarini <fabio.chiarini@studio.unibo.it>
2021-02-12 17:39:17 +0100

    Update MonsterType.java

--- src/main/java/rogue/model/creature/MonsterType.java
+++ src/main/java/rogue/model/creature/MonsterType.java
@@ -80,7 +80,77 @@ public enum MonsterType {
      * Nymph.
      */
     NYMPH(new MonsterLife((int) (Math.random() * (24 - 3) + 3), 37), 9,  new Pair<>(0, 0),
-            new SpecialImpl(false, false, true, false, false, false, false, false), 111,  new PotionImpl(PotionType.POTION_V), 50);
+            new SpecialImpl(false, false, true, false, false, false, false, false), 111,  new PotionImpl(PotionType.POTION_V), 50),
+
+    /** 
+     * Orc.
+     */
+    ORC(new MonsterLife((int) (Math.random() * (8 - 1) + 1), 5), 6,  new Pair<>(1, 8),
+            new SpecialImpl(false, false, true, false, false, false, false, false), 500),
+
+    /** 
+     * Phanthom.
+     */
+    PHANTHOM(new MonsterLife((int) (Math.random() * (64 - 8) + 8), 120), 3,  new Pair<>(4, 16),
+            new SpecialImpl(true, false, false, true, false, false, false, false), 0),
+
+    /** 
+     * Quaqqa.
+     */
+    QUAQQA(new MonsterLife((int) (Math.random() * (24 - 3) + 3), 15), 3,  new Pair<>(2, 10),
+            new SpecialImpl(), 0),
+
+    /** 
+     * Ratfolk.
+     */
+    RATFOLK(new MonsterLife((int) (Math.random() * (16 - 2) + 2), 9), 3,  new Pair<>(1, 6), new SpecialImpl(), 27, new PotionImpl(PotionType.POTION_II), 60),
+
+    /** 
+     * Snake.
+     */
+    SNAKE(new MonsterLife((int) (Math.random() * (8 - 1) + 1), 2), 5,  new Pair<>(1, 3),
+            new SpecialImpl(true, false, false, false, false, false, true, false), 6),
+
+    /** 
+     * Troll.
+     */
+    TROLL(new MonsterLife((int) (Math.random() * (48 - 6) + 6), 120), 4,  new Pair<>(3, 24),
+            new SpecialImpl(true), 360,  new PotionImpl(PotionType.POTION_IV), 70),
+
+    /** 
+     * Unicorn.
+     */
+    UNICORN(new MonsterLife((int) (Math.random() * (56 - 7) + 7), 190), 3,  new Pair<>(4, 36),
+            new SpecialImpl(), 570,  new PotionImpl(PotionType.CORRUPT_POTION_II), 100),
+
+    /** 
+     * Vampire.
+     */
+    VAMPIRE(new MonsterLife((int) (Math.random() * (64 - 8) + 8), 350), 1,  new Pair<>(1, 10),
+           new SpecialImpl(true, false, false, false, false, false, false, true), 1050,  new PotionImpl(PotionType.POTION_IV), 100),
+
+    /** 
+     * Wraith.
+     */
+    WRAITH(new MonsterLife((int) (Math.random() * (40 - 5) + 5), 55), 4,  new Pair<>(1, 6), new SpecialImpl(), 0,  new PotionImpl(PotionType.CORRUPT_POTION_I), 60),
+
+    /** 
+     * Xill.
+     */
+    XILL(new MonsterLife((int) (Math.random() * (72 - 8) + 8), 50), 2,  new Pair<>(3, 24),
+            new SpecialImpl(true), 150,  new PotionImpl(PotionType.POTION_IV), 20),
+
+    /** 
+     * Yeti.
+     */
+    YETI(new MonsterLife((int) (Math.random() * (32 - 4) + 4), 100), 6,  new Pair<>(2, 12),
+           new SpecialImpl(), 900,  new PotionImpl(PotionType.POTION_III), 40),
+
+    /** 
+     * Zombie.
+     */
+    ZOMBIE(new MonsterLife((int) (Math.random() * (24 - 3) + 3), 6), 8,  new Pair<>(2, 16),
+            new SpecialImpl(true, false, false, false, false, true, false, false), 16,  new PotionImpl(PotionType.CORRUPT_POTION_II), 50);
 
 
     private final MonsterLife life;
