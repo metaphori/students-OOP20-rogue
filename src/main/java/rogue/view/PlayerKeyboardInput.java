12c5e37291424a2254f43d7926f7be0ce6df006f
Luca Tassinari <luca.tassinari10@studio.unibo.it>
2021-02-13 08:55:01 +0100

    Updated computeDirection method

--- src/main/java/rogue/view/PlayerKeyboardInput.java
+++ src/main/java/rogue/view/PlayerKeyboardInput.java
@@ -1,7 +1,13 @@
 package rogue.view;
 
+import static javafx.scene.input.KeyCode.*;
+
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javafx.geometry.Point2D;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
@@ -11,6 +17,8 @@ import rogue.controller.PlayerInputController;
  * Manages keyboard inputs.
  */
 public final class PlayerKeyboardInput {
+    
+    private static final Logger LOG = LoggerFactory.getLogger(PlayerKeyboardInput.class);
 
     private Optional<KeyCode> keyPressed = Optional.empty();
     private Optional<PlayerInputController> listener = Optional.empty();
@@ -22,22 +30,26 @@ public final class PlayerKeyboardInput {
      */
     public PlayerKeyboardInput(final Scene scene) {
         scene.addEventHandler(KeyEvent.KEY_PRESSED, this::onKeyPressed);
-        scene.addEventHandler(KeyEvent.KEY_RELEASED, this::onKeyReleased);
+        scene.addEventHandler(KeyEvent.KEY_RELEASED, this::onKeyPressed);
     }
 
     private void onKeyPressed(final KeyEvent event) {
         this.keyPressed = Optional.of(event.getCode());
+        this.computeDirection();
     }
 
-    private void onKeyReleased(final KeyEvent event) {
-        this.keyPressed = Optional.empty();
-    }
-
-    // TODO
-    private void computeDirection() {
-    }
-
-    private void notifyListener() {
+    private Point2D computeDirection() {
+        final KeyCode key = this.keyPressed.get();
+        if (key == LEFT || key == H) {
+            LOG.info("left");
+        } else if (key == RIGHT || key == L) {
+            LOG.info("right");
+        } else if (key == UP || key == K) {
+            LOG.info("up");
+        } else if (key == DOWN || key == J) {
+            LOG.info("down");
+        }
+        return Point2D.ZERO;
     }
 
 }
