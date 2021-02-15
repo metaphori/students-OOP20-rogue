36bc95ba568b50118522b1e08082928ed73734f8
Luca Tassinari <luca.tassinari10@studio.unibo.it>
2021-02-15 12:52:11 +0100

    Adjusted constructor and added onLifeChange method subscription

--- src/main/java/rogue/controller/PlayerController.java
+++ src/main/java/rogue/controller/PlayerController.java
@@ -1,15 +1,34 @@
 package rogue.controller;
 
-import rogue.model.Entity;
-import rogue.view.EntityView;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.eventbus.Subscribe;
+
+import rogue.model.creature.Player;
+import rogue.model.events.EventSubscriber;
+import rogue.model.events.LifeEvent;
+import rogue.view.PlayerView;
 
 /**
  * The player controller.
  */
-public final class PlayerController extends AbstractEntityController {
+public final class PlayerController extends AbstractEntityController implements EventSubscriber {
 
-    protected PlayerController(final EntityView view, final Entity entity) {
-        super(view, entity);
+    private static final Logger LOG = LoggerFactory.getLogger(PlayerController.class);
+
+    protected PlayerController(final PlayerView playerView, final Player player) {
+        super(playerView, player);
+        player.getLife().register(this);
+    }
+
+    /**
+     * 
+     * @param event
+     */
+    @Subscribe
+    public void onLifeChange(final LifeEvent event) {
+        LOG.info("life changed");
     }
 
 }
