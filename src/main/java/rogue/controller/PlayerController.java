f8f4a9e72cff8eabd07aab014f16c764e01d6080
Luca Tassinari <luca.tassinari10@studio.unibo.it>
2021-02-15 09:41:14 +0100

    Created PlayerController

---
+++ src/main/java/rogue/controller/PlayerController.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package rogue.controller;
+
+import rogue.model.Entity;
+import rogue.view.EntityView;
+
+/**
+ *
+ */
+public final class PlayerController extends AbstractEntityController {
+
+    protected PlayerController(final EntityView view, final Entity entity) {
+        super(view, entity);
+    }
+
+}
