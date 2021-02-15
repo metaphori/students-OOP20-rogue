bdf95e805581526daaef3edea6608508dcc96d92
Luca Tassinari <luca.tassinari10@studio.unibo.it>
2021-02-15 16:00:52 +0100

    Added EquipmentEvent

---
+++ src/main/java/rogue/model/events/EquipmentEvent.java
@@ -0,0 +1,28 @@
+package rogue.model.events;
+
+import rogue.model.items.Equipment;
+
+/**
+ * A class representing an equipment change event.
+ */
+public class EquipmentEvent implements EntityEvent {
+
+    private final Equipment equipment;
+
+    /**
+     * Creates a new EquipmentEvent.
+     * @param equipment
+     *          the equipment which changed
+     */
+    public EquipmentEvent(final Equipment equipment) {
+        this.equipment = equipment;
+    }
+
+    /**
+     * @return the event related {@link Equipment} 
+     */
+    public Equipment getEquipment() {
+        return this.equipment;
+    }
+
+}
