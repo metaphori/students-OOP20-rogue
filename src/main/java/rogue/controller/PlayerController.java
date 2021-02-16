25ec8499b7d25b354562d88aa33376ad0f353a3a
Luca Tassinari <luca.tassinari10@studio.unibo.it>
2021-02-16 14:59:47 +0100

    Added javadoc and view update

--- src/main/java/rogue/controller/PlayerController.java
+++ src/main/java/rogue/controller/PlayerController.java
@@ -15,36 +15,52 @@ import rogue.view.StatusBarView;
 /**
  * The player controller.
  */
-public final class PlayerController extends AbstractEntityController implements EventSubscriber {
+public final class PlayerController extends AbstractEntityController<StatusBarView> implements EventSubscriber {
 
     private static final Logger LOG = LoggerFactory.getLogger(PlayerController.class);
 
-    protected PlayerController(final StatusBarView statusBarView, final Player player) {
+    /**
+     * Creates a new PlayerController.
+     * @param statusBarView
+     *          the {@link StatusBarView} which displays the player life's statistics
+     * @param player
+     *          the {@link Player} entity
+     */
+    public PlayerController(final StatusBarView statusBarView, final Player player) {
         super(statusBarView, player);
         player.getLife().register(this);
         player.getEquipment().register(this);
+        // Sets the initial score view!
+        this.onLifeChange(new LifeEvent<>(player.getLife()));
     }
 
     /**
-     * 
+     * TODO to refactor...
+     * Notifies that the {@link PlayerLife} changed.
      * @param event
+     *          the {@link LifeEvent} associated to the life change
      */
     @Subscribe
     public void onLifeChange(final LifeEvent<PlayerLife> event) {
-        LOG.info("life changed " + event.getLife().getHealthPoints());
-        // TODO: update status bar view
+        LOG.info("Life changed");
+        this.getView().setCoins(event.getLife().getCoins());
+        this.getView().setCurrentHealthPoints(event.getLife().getHealthPoints());
+        this.getView().setMaxHealthPoints(event.getLife().getMaxHealthPoints());
+        this.getView().setExperience(event.getLife().getExperience());
+        this.getView().setLevel(event.getLife().getLevel());
+        this.getView().setStrength(event.getLife().getStrength());
     }
 
     /**
-     * 
+     * TODO to refactor...
+     * Notifies that the {@link Equipment} changed.
      * @param event
+     *          the {@link EquipmentEvent} associated to the equipment change
      */
     @Subscribe
     public void onEquipmentChange(final EquipmentEvent event) {
-        LOG.info("equipment changed ");
-        LOG.info("Armor: " + event.getEquipment().getArmor());
-        LOG.info("Weapon: " + event.getEquipment().getWeapon());
-        // TODO: update equipment view
+        LOG.info("Equipment changed");
+        // TODO
     }
 
 }
