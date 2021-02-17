377847b62b5cdc6c9578b4cf3578fe3dc27bbd09
Luca Tassinari <luca.tassinari10@studio.unibo.it>
2021-02-17 20:33:12 +0100

    Improved gui and code

--- src/main/java/rogue/view/GameView.java
+++ src/main/java/rogue/view/GameView.java
@@ -4,11 +4,8 @@ import java.awt.Dimension;
 import java.io.IOException;
 
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.SplitPane;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -21,11 +18,10 @@ import rogue.view.inventory.InventoryViewImpl;
 public class GameView {
 
     private final Stage stage = new Stage();
-    private final Parent root;
     private final Scene scene;
 
     private void loadStatusBar() {
-        final Pane pane = (Pane) this.scene.lookup("#statusBarPane");
+        final Pane pane = (Pane) this.scene.lookup("#status");
         System.out.print(pane);
 
         pane.getChildren().add(new StatusBarViewImpl().getNode());
@@ -52,7 +48,7 @@ public class GameView {
         final double width = screenSize.getWidth();
         final double height = screenSize.getHeight();
 
-        this.root = FXMLLoader.load(ClassLoader.getSystemResource("layout/MainView.fxml"));
+        final Parent root = FXMLLoader.load(ClassLoader.getSystemResource("layout/MainView.fxml"));
         this.scene = new Scene(root, width, height);
 
         this.loadStatusBar();
@@ -64,23 +60,5 @@ public class GameView {
         stage.setResizable(true);
         stage.show();
     }
-
-//    public GameView() throws IOException {
-//        this.root.getChildren().add(new StatusBarViewImpl().getNode());
-//        final FXMLLoader loader = new FXMLLoader(ClassLoader.getSystemResource("layout/InventoryView.fxml"));
-//        this.root.getChildren().add(loader.load());
-//
-//        final Player player = new PlayerFactoryImpl().create();
-//        final InventoryController inventoryController = new InventoryControllerImpl(player);
-//
-//        final InventoryViewImpl controller = loader.getController();
-//        controller.init(inventoryController);
-//
-//        stage.setScene(this.scene);
-//        stage.getIcons().add(new Image(ClassLoader.getSystemResource("images/rogueIcon.png").toExternalForm()));
-//        stage.setTitle("Rogue");
-//        stage.setResizable(true);
-//        stage.show();
-//    }
 
 }
