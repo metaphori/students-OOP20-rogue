c394cd930a7285afc153d85024f594617a91bc29
Manuel Quarneti <manuelquarneti@gmail.com>
2021-02-18 19:42:52 +0100

    refactoring

--- src/main/java/rogue/controller/WorldController.java
+++ src/main/java/rogue/controller/WorldController.java
@@ -1,12 +1,29 @@
 package rogue.controller;
 
+import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import rogue.model.Game;
+import rogue.model.GameImpl;
+import rogue.model.creature.Player;
 import rogue.model.world.Direction;
+import rogue.view.WorldScene;
 
-public class MovePlayer {
-    public static void movePlayer(KeyEvent event, Game game) {
+public class WorldController {
+    private static final int DEPTH = 5;
+    private final Game game;
+    private final WorldScene worldScene;
+
+    public WorldController(Player player) {
+        this.game = new GameImpl(DEPTH, player);
+        this.worldScene = new WorldScene(game);
+    }
+
+    public WorldScene getWorldScene() {
+        return worldScene;
+    }
+
+    public void movePlayer(KeyEvent event) {
         final KeyCode key = event.getCode();
 
         Direction direction = Direction.NONE;
@@ -33,5 +50,6 @@ public class MovePlayer {
         }
 
         game.round(direction);
+        worldScene.drawMap();
     }
 }
