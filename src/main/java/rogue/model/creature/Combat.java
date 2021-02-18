f120d2d779fb1156a4f4cc53ce242493fd4c7095
Fabio Chiarini <fabio.chiarini@studio.unibo.it>
2021-02-18 16:09:41 +0100

    Update Combat.java

--- src/main/java/rogue/model/creature/Combat.java
+++ src/main/java/rogue/model/creature/Combat.java
@@ -6,13 +6,42 @@ package rogue.model.creature;
  */
 public interface Combat {
 
+    enum Result {
+
+       /** 
+        * Miss if the attacker miss the defender.
+        */
+        MISS,
+
+        /** 
+         * Hit if the attacker hit the defender.
+         */
+        HIT,
+
+        /** 
+         * Poison if the attacker hit the defender and did another damage with poison.
+         */
+        POISON,
+
+        /** 
+         * Poison if the attacker hit the defender and he steal health points.
+         */
+        DRAINLIFE,
+
+        /** 
+         * Dead if the attacker hit the defender and the defender is dead.
+         */
+        DEAD;
+     }
+
     /**
      * 
      * @param attacker
      *          who made the attack
      * @param defender
      *         who defends himself from the attack
+     * @return the result of the attack
      */
-    void attack(Creature<?> attacker, Creature<?> defender);
+     Result attack(Creature<?> attacker, Creature<?> defender);
 
 }
