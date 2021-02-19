2a6a95f5b3fc798d1028e5ece76dc415d0d74170
Manuel Quarneti <manuelquarneti@gmail.com>
2021-02-19 17:02:49 +0100

    tile javadoc

--- src/main/java/rogue/model/world/Tile.java
+++ src/main/java/rogue/model/world/Tile.java
@@ -1,10 +1,31 @@
 package rogue.model.world;
 
+/**
+ * a tile on the map.
+ */
 public interface Tile {
-    Level getLevel();
+    /**
+     * @return the x coordinate
+     */
     int getX();
+
+    /**
+     * @return the y coordinate
+     */
     int getY();
+
+    /**
+     * @return the tile's material
+     */
     Material getMaterial();
+
+    /**
+     * set the tile's material as DOOR.
+     */
     void setDoor();
+
+    /**
+     * @return if the tile is a wall
+     */
     boolean isWall();
 }
