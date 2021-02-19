a8a3fb0cd52a9bdaa443f8946d1d2370c34f0fe0
Manuel Quarneti <manuelquarneti@gmail.com>
2021-02-19 16:38:34 +0100

    javadoc

--- src/main/java/rogue/model/world/Direction.java
+++ src/main/java/rogue/model/world/Direction.java
@@ -1,5 +1,31 @@
 package rogue.model.world;
 
+/**
+ * Just the cardinal direction.
+ */
 public enum Direction {
-    NONE, NORTH, EAST, SOUTH, WEST
+    /**
+     * NONE special direction.
+     */
+    NONE,
+
+    /**
+     * NORTH cardinal direction.
+     */
+    NORTH,
+
+    /**
+     * EAST cardinal direction.
+     */
+    EAST,
+
+    /**
+     * SOUTH cardinal direction.
+     */
+    SOUTH,
+
+    /**
+     * WEST cardinal direction.
+     */
+    WEST
 }
