08e3195032aa9914194cebd76ec04bad800d1ca8
Manuel Quarneti <manuelquarneti@gmail.com>
2021-02-19 17:01:15 +0100

    javadoc

--- src/main/java/rogue/model/world/Material.java
+++ src/main/java/rogue/model/world/Material.java
@@ -1,5 +1,21 @@
 package rogue.model.world;
 
+/**
+ * the tile's material.
+ */
 public enum Material {
-    BRICKS, VINES, DOOR
+    /**
+     * just bricks.
+     */
+    BRICKS,
+
+    /**
+     * fancier bricks.
+     */
+    VINES,
+
+    /**
+     * the door to the next level.
+     */
+    DOOR
 }
