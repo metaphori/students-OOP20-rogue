d20424e82379b92156c7c087061425ae2fb3a1f2
Manuel Quarneti <manuelquarneti@gmail.com>
2021-02-19 16:16:45 +0100

    do not cache previous levels. refactoring

--- src/main/java/rogue/model/GameImpl.java
+++ src/main/java/rogue/model/GameImpl.java
@@ -1,13 +1,13 @@
 package rogue.model;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
-import java.util.function.Supplier;
 import java.util.stream.Stream;
 
+import rogue.model.creature.MonsterFactory;
 import rogue.model.creature.MonsterFactoryImpl;
 import rogue.model.creature.Player;
+import rogue.model.items.ItemFactory;
 import rogue.model.items.ItemFactoryImpl;
 import rogue.model.world.Direction;
 import rogue.model.world.Level;
@@ -15,35 +15,38 @@ import rogue.model.world.LevelImpl;
 import rogue.model.world.Tile;
 
 public class GameImpl implements Game {
-    private List<Level> levels = new ArrayList<>();
+    private static final MonsterFactory MONSTER_FACTORY = new MonsterFactoryImpl();
+    private static final ItemFactory ITEM_FACTORY = new ItemFactoryImpl();
+
+    private Level currentLevel;
     private Player player;
 
     private final Runnable nextLevel = () -> {
         var entityList = new ArrayList<Entity>();
         entityList.add(player);
-        entityList.addAll(new MonsterFactoryImpl().createMonsterList(player.getLife().getLevel()));
-        entityList.addAll(new ItemFactoryImpl().getItems());
+        entityList.addAll(MONSTER_FACTORY.createMonsterList(player.getLife().getLevel()));
+        entityList.addAll(ITEM_FACTORY.getItems());
 
-        levels.add(new LevelImpl(entityList, player));
+        currentLevel = new LevelImpl(entityList, player);
     };
 
-    private final Supplier<Level> getCurrentLevel = () -> levels.get(levels.size() - 1);
-
     public final Stream<Tile> getTiles() {
-        return getCurrentLevel.get().getTileStream();
+        return currentLevel.getTileStream();
     }
 
     public final Map<Entity, Tile> getEntityMap() {
-        return getCurrentLevel.get().getEntityMap();
+        return currentLevel.getEntityMap();
     }
 
-    /** round.
+    /**
+     * round.
+     * 
      * @param direction player movement direction
      * @return nextLevel?
      */
     public final boolean round(final Direction direction) {
         boolean nextlvl = false;
-        if (getCurrentLevel.get().moveEntities(direction)) {
+        if (currentLevel.moveEntities(direction)) {
             nextLevel.run(); // change level
             nextlvl = true;
         }
@@ -55,11 +58,11 @@ public class GameImpl implements Game {
     }
 
     public final int getWidth() {
-        return getCurrentLevel.get().getWidth();
+        return currentLevel.getWidth();
     }
 
     public final int getHeight() {
-        return getCurrentLevel.get().getHeight();
+        return currentLevel.getHeight();
     }
 
     public GameImpl(final int depth, final Player player) {
