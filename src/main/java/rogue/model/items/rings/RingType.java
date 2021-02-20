0d800d095ef2150ef21de328e2ca4b5647481d7a
Luca Tassinari <luca.tassinari10@studio.unibo.it>
2021-02-20 17:48:08 +0100

    Removed not useful chance int value

--- src/main/java/rogue/model/items/rings/RingType.java
+++ src/main/java/rogue/model/items/rings/RingType.java
@@ -10,33 +10,29 @@ import rogue.model.items.weapons.IncreaseDamage;
 /**
  * Represents an enumeration for declaring ring types.
  * 
- * The first field keeps track the percentage of chance that the corresponding ring has of being found.
- * The second field keeps track a consumer describing its effect on the player.
- *
+ * The field keeps track a consumer describing its effect on the player.
  */
 public enum RingType {
 
     /**
      * Increase armor AC by 2 points.
      */
-    PROTECTION(9, e -> { 
+    PROTECTION(e -> { 
         e.setArmor(new ArmorImpl(e.getArmor().getArmorType())); 
         e.getArmor().increaseAC(2); 
     }),
     /**
      * Improves weapon accuracy.
      */
-    DEXTERITY(8, e -> e.setWeapon(new IncreaseAccuracy(e.getWeapon()))),
+    DEXTERITY(e -> e.setWeapon(new IncreaseAccuracy(e.getWeapon()))),
     /**
      * Increases weapon damage.
      */
-    INCREASE_DAMAGE(8, e -> e.setWeapon(new IncreaseDamage(e.getWeapon())));
+    INCREASE_DAMAGE(e -> e.setWeapon(new IncreaseDamage(e.getWeapon())));
 
-    private final int chance;
     private final Consumer<Equipment> consumer;
 
-    RingType(final int chance, final Consumer<Equipment> consumer) {
-        this.chance = chance;
+    RingType(final Consumer<Equipment> consumer) {
         this.consumer = consumer;
     }
 
@@ -45,13 +41,6 @@ public enum RingType {
      */
     protected Consumer<Equipment> getConsumer() {
         return this.consumer;
-    }
-
-    /**
-     * @return the ring chance of being found
-     */
-    protected int getChance() {
-        return this.chance;
     }
 
 }
